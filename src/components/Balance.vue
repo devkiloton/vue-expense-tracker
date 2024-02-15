27d1da6cb0f16c0e58eab09ab8c6789a8de7701e
devkiloton <dev.kiloton@gmail.com>
2024-02-15 00:04:32 -0300

    Developing components without functionality

--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,8 +1,11 @@
 <template>
-  <div></div>
+  <h4>Your balance</h4>
+  <h1 id="balance">$0.00</h1>
 </template>
 
 <script>
+/* eslint-disable */
+
 export default {
   name: "balance",
   created() {},
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,9 @@
 <template>
-  <div></div>
+  <h2>Expense tracker</h2>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
   name: "header",
   created() {},

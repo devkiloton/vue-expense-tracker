2284d41be3d134a3815da552c54a73b26132aaa9
devkiloton <dev.kiloton@gmail.com>
2024-02-15 00:52:03 -0300

    Creating props and iterating through transactions

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,25 +1,26 @@
+<!-- eslint-disable prettier/prettier -->
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
-      Cash <span>-$400</span><button class="delete-btn">x</button>
-    </li>
-    <li class="plus">
-      Paycheck <span>+$800</span><button class="delete-btn">x</button>
+    <li
+      v-for="transaction in props.transactions"
+      v-bind:key="transaction.id"
+      v-bind:class="transaction.amount < 0 ? 'minus' : 'plus'">
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
 
-<script>
-export default {
-  name: "transaction-list",
-  created() {},
-  data() {
-    return {};
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
   },
-  props: {},
-  methods: {},
-};
+});
 </script>
 
 <style lang="scss" scoped></style>

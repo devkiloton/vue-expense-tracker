d52e813d0ddd8a45af7c5dc4c46bc1a851689d8c
devkiloton <dev.kiloton@gmail.com>
2024-02-15 02:23:49 -0300

    Collecting and emitting data

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,30 +1,48 @@
 <template>
   <h3>Add new transaction</h3>
-  <form id="form">
+  <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="text">Text</label>
-      <input type="text" id="text" placeholder="Enter text..." />
+      <input type="text" id="text" placeholder="Enter text..." v-model="text" />
     </div>
     <div class="form-control">
       <label for="amount"
         >Amount <br />
         (negative - expense, positive - income)</label
       >
-      <input type="number" id="amount" placeholder="Enter amount..." />
+      <input
+        type="number"
+        id="amount"
+        placeholder="Enter amount..."
+        v-model="amount" />
     </div>
     <button class="btn">Add transaction</button>
   </form>
 </template>
 
-<script>
-export default {
-  name: "add-transaction",
-  created() {},
-  data() {
-    return {};
-  },
-  props: {},
-  methods: {},
+<script setup>
+import { ref, defineEmits } from "vue";
+import { useToast } from "vue-toastification";
+
+const text = ref("");
+const amount = ref("");
+const toast = useToast();
+const emit = defineEmits(["transactionSubmitted"]);
+
+const onSubmit = () => {
+  if (!text.value || !amount.value) {
+    toast.error("Both fields must to be filled");
+  }
+
+  const transactionData = {
+    text: text.value,
+    amount: amount.value,
+  };
+
+  emit("transactionSubmitted", transactionData);
+
+  text.value = "";
+  amount.value = "";
 };
 </script>
 

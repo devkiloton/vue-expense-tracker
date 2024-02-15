ff5bcfe69a3309d9bae11b354de30e65723042a0
devkiloton <dev.kiloton@gmail.com>
2024-02-14 23:06:10 -0300

    Setting up the components I'll work with

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "header",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "income-expenses",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>

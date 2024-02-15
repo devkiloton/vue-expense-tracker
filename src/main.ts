495780737123810bd92b045ca373d13d5a0c52f0
devkiloton <dev.kiloton@gmail.com>
2024-02-15 01:51:30 -0300

    Adding notification's plugin

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/style.scss";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+// Adding plugin for notifications
+app.use(Toast);
+app.mount("#app");
